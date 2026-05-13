39724f6d372b06baee87d147db69162fa89f955e
Leo <tungphamtp987@gmail.com>
2026-05-13 22:38:24 +0700

    texxt

--- src/app/components/RoadMap.js
+++ src/app/components/RoadMap.js
@@ -165,7 +165,7 @@ export default function RoadMap() {
       name: t("goal_name_6"),
       icon: <Users className="w-6 h-6" />,
       color: "pink",
-      images: ["/images/roadmap/Phòng Chế Tạo/ns.jpg"],
+      images: ["/images/roadmap/Phòng Chế Tạo/nhansu.jpg"],
       objectives: [
         {
           title: t("sub_goal_name_6_1"),

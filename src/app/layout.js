65e5528e2e283d59ecc0b74d31af582261b82d63
Leo <tungphamtp987@gmail.com>
2026-06-16 13:03:00 +0700

    meta

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,8 +17,18 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "SEV",
-  description: "SEV",
+  title: "SEV EV giới thiệu",
+  description: `Soltec Engineering Vietnam phiên bản mới đã chính thức khởi động.
+
+Chúng tôi đã tái cấu trúc và xây dựng cơ sở sản xuất tại Việt Nam, nhằm cung cấp kịp thời các sản phẩm có giá trị gia tăng cao dựa trên năng lực kỹ thuật chế tạo. Đồng thời, chúng tôi cũng đã đạt được các giấy phép gia công có thể đáp ứng tiêu chuẩn toàn cầu, qua đó đáp ứng kỳ vọng của Quý khách hàng.
+
+Bên cạnh việc thiết kế và chế tạo các thiết bị như tháp, bồn, thiết bị trao đổi nhiệt và các loại máy móc công nghiệp, chúng tôi còn cung cấp dịch vụ trọn gói từ lắp đặt tại chỗ đến bảo trì. Phát huy những thành tựu đã đạt được, chúng tôi sẽ tiếp tục nỗ lực đáp ứng nhu cầu của khách hàng dựa trên QCDS (Chất lượng – Chi phí – Tiến độ – An toàn).
+
+Hơn nữa, với việc trở thành một thành viên của Tập đoàn Soltec, thông qua sự phối hợp giữa công ty mẹ tại Nhật Bản, Soltec Vietnam và các đơn vị trong tập đoàn như Rutenchem, chúng tôi sẽ phát huy sức mạnh tổng hợp và nâng cao giá trị cung cấp đến khách hàng.
+
+Không chỉ tại Nhật Bản, chúng tôi còn hướng tới cung cấp kịp thời các sản phẩm có giá trị gia tăng cao với năng lực sản xuất vượt trội đến khu vực ASEAN và các thị trường quốc tế khác.
+
+Toàn thể cán bộ nhân viên sẽ đồng lòng nỗ lực hết mình. Kính mong Quý vị tiếp tục tin tưởng và kỳ vọng vào chúng tôi.`,
 };
 
 export default function RootLayout({ children }) {
